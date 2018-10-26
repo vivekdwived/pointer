d5108f5577c64854c4def9a2e63c10d981fd7e27
vivekdwived <42137907+vivekdwived@users.noreply.github.com>
2018-10-26 10:12:28 -0400

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{	
+	char s[6]="abcde";
+	char*cptr;
+	cptr=&s[6];
+	cout<<*cptr[]
+return 0;
+}
+
+
---
+++ pointer1 to 4.cpp
@@ -0,0 +1,31 @@
+	
+	//question pointer1 OUTPUT
+
+	*p3=S
+	*p3=T,p3=6940
+	*p=S,p1=6940
+
+	//question 2 POINTER OUTPUT
+	option D
+
+	//question 3 pointer OUTPUT
+
+	double *p ---means a box created stores addess of another box HAVING A DOUBLE TYPE OF DATA...
+	but here "c" --is a character
+
+	so, error is SYNTAX ERROR.
+
+	//question 4 pointer OUTPUT
+
+	temp=A
+	temp=C
+	temp=B
+	temp=A
+
+	temp=B
+	temp=C
+
+	temp=B
+	temp=C
+	temp=C
+	temp=C
